9153773845135b9d33f8f31a0694888a86684aba
Daniil Benger <torigetz@yandex.ru>
2024-04-01 17:24:42 +0300

    chore(main.tsx): add effector logger

--- src/main.tsx
+++ src/main.tsx
@@ -1,5 +1,10 @@
 import ReactDOM from 'react-dom/client'
+import { attachLogger } from 'effector-logger';
 import { App } from '~/app';
+
+if (import.meta.env.MODE !== 'production') {
+  attachLogger();
+}
 
 ReactDOM.createRoot(
   document.getElementById('root') as HTMLElement,
